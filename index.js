ee81292126ae52a5d8cbd203cd42ac0494cfe332
Bernard Maina <bmburu@innovexsolutions.co.ke>
2023-11-23 10:34:57 +0300

    ~ fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let crypto = require("crypto");
 let id = crypto.randomBytes(20).toString('hex');
 let generatedAt = new Date().getTime();
-let randomHash = "This hash was generated at:" + generatedAt + ":" + id;
+let randomHash = "Time:" + generatedAt + " " + "Hash:" + id;
 console.log(randomHash);
